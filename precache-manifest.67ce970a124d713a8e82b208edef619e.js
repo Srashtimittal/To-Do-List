785ff3708ec8b9cc637e3803b4504bc5a825e478
Srashti <srashtimittal990@gmail.com>
2020-10-25 22:50:44 +0530

    Updates

---
+++ precache-manifest.67ce970a124d713a8e82b208edef619e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43439d8f410643c4f812e4c18728172",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "66075878906b123f1c2a",
+    "url": "/To-Do-List/static/css/main.70999be4.chunk.css"
+  },
+  {
+    "revision": "3fa0ea1d91bf2a5d87ac",
+    "url": "/To-Do-List/static/js/2.a2053ab5.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/To-Do-List/static/js/2.a2053ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66075878906b123f1c2a",
+    "url": "/To-Do-List/static/js/main.2cb80514.chunk.js"
+  },
+  {
+    "revision": "e71b0fca7b414be357cd",
+    "url": "/To-Do-List/static/js/runtime-main.8e2d492d.js"
+  }
+]);
